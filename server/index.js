cca85925f6b2c6e967bb2db9965d2983e4a80a06
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2017-07-18 12:01:31 +0300

    preparation for heroku deploy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+
 let tickets = require('./data/tickets.mock.json')
 
-// const port = 3000
+const port = 3003
+app.set('port', port)
 
 app.get('/api/tickets', (req, res) => {
   setTimeout(() => res.send(tickets), 750)
@@ -15,13 +17,11 @@ app.delete('/api/tickets/:id', (req, res) => {
   setTimeout(() => res.send({status: 'ok'}), 500)
 })
 
-// Define the port to run on
-app.set('port', 3000)
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.use('/', express.static(path.join(__dirname + '/public')))
+}
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', express.static(path.join(__dirname + '/public')))
-// Listen for requests
-const server = app.listen(app.get('port'), function () {
-  const port = server.address().port
-  console.log('Magic happens on port ' + port)
+const server = app.listen(app.get('port'), () => {
+  console.log('API server starts on port ' + port)
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,99 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const extractSass = new ExtractTextPlugin({
-  filename: "[name].[contenthash].css",
-  disable: process.env.NODE_ENV === "development"
-})
-module.exports = {
-  entry: [
-    './src/index.js'
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-  ],
-  output: {
-    path: __dirname + "/public",
-    filename: 'bundle.js'
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
-        use: 'url-loader'
-      }, {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['react', 'es2015', 'stage-1'],
-            plugins: ['transform-decorators-legacy']
-          }
-        }
-      }, {
-        test: /\.sass$/,
-        use: [{
-          loader: "style-loader" // creates style nodes from JS strings
-        }, {
-          loader: "css-loader", options: {
-            sourceMap: true
-          } // translates CSS into CommonJS
-        }, {
-          loader: "sass-loader", options: {
-            sourceMap: true
-          } // compiles Sass to CSS
-        }]
-      }, {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml'},
-      {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
-      {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/octet-stream"},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"}]
+let env = process.env.NODE_ENV || 'development';
+if (env !== 'production') {
+  env = 'development';
+}
 
-  },
-  plugins: [
-    extractSass,
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+const _module = {
+  rules: [{
+    test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
+    use: 'url-loader'
+  }, {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-1'],
+        plugins: ['transform-decorators-legacy']
       }
-    }),
+    }
+  }, {
+    test: /\.sass$/,
+    use: [{
+      loader: 'style-loader' // creates style nodes from JS strings
+    }, {
+      loader: 'css-loader',
+      options: {
+        sourceMap: true
+      } // translates CSS into CommonJS
+    }, {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: true
+      } // compiles Sass to CSS
+    }]
+  }, { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml' },
+    { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff' },
+    { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff' },
+    { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/octet-stream' },
+    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' }
+  ]
+};
+
+let _plugins = [
+  new ExtractTextPlugin({
+    filename: '[name].[contenthash].css',
+    disable: env === 'development'
+  }),
+  new HtmlWebpackPlugin({
+    template: __dirname + '/index.html'
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify(env)
+    }
+  })
+];
+
+if (env === 'production') {
+  _plugins.unshift(
+    new CleanWebpackPlugin([__dirname + '/server/public/*.*'], { verbose: true, dry: false })
+  );
+  _plugins.push(
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
       }
-    }),
-    new HtmlWebpackPlugin({
-      template: __dirname + '/index.html'
     })
+  );
+};
+
+module.exports = {
+  entry: [
+    './src/index.js'
   ],
+  output: {
+    path: __dirname + '/server/public',
+    filename: 'bundle.js'
+  },
+  devtool: env !== 'production' ? 'source-map' : false,
+  module: _module,
+  plugins: _plugins,
   resolve: {
     extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './',
+    port: 5005,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3003',
         secure: false
       }
     }
