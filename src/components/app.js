960db269b94c47012d4b7e17e2fdd0018feda7c7
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2017-07-25 22:44:37 +0300

    router initied

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Dashboard from './Dashboard'
+
+const Archive = () => (<h2>This is Archive Page</h2>)
 
 export default class App extends Component {
   render() {
     return (
-      <Dashboard />
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => (<h1>Home page</h1>)}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+          <Route path="/archive" component={Archive}/>
+        </Switch>
+      </Router>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
-import rootReducer from './reducers/index'
+import store from './store'
 import App from './components/app'
 import '../style/app.sass'
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk, logger)
-)
 
 ReactDOM.render(
   <Provider store={store}>
